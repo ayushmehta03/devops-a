a6fc9c0cd17719b5c89a98414c262266960434a6
Ayush Mehta <ayushmehtabps@gmail.com>
2026-06-15 17:14:14 +0530

    add tf file for remote_backend servers

---
+++ terraform/aws/remote_state/main.tf
@@ -0,0 +1,52 @@
+
+terraform {
+required_version = ">= 1.0"
+
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 5.0"
+}
+}
+}
+
+provider "aws" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+bucket = "${local.account_id}-terraform-states"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+bucket = aws_s3_bucket.terraform_state.id
+
+versioning_configuration {
+status = "Enabled"
+}
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+bucket = aws_s3_bucket.terraform_state.id
+
+rule {
+apply_server_side_encryption_by_default {
+sse_algorithm = "AES256"
+}
+}
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+name = "terraform-lock"
+billing_mode = "PAY_PER_REQUEST"
+hash_key = "LockID"
+
+attribute {
+name = "LockID"
+type = "S"
+}
+}
